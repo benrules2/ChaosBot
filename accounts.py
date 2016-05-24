0eef3b1364c181683bbe619f2088c80491e52244
benrules2 <bceagan@gmail.com>
2016-05-23 21:56:44 -0400

    Remove key

--- accounts.py
+++
@@ -1,4 +0,0 @@
-twitter_account = {'consumer_key':'CZsP4fZ6MecjjC7vJvJosytsa',
-'consumer_secret':'w4GQS9f4zXiFOKld7uiVltRQbwKtyk1K5XXKZiqnYiTDixZrbe',
-'access_key':'720030757587013633-A1RX2HOfGjpc8IHFuhVs1HpwioYT5F3',
-'access_secret' : 'rCIg7PhbfkcZh941e55zbQDabtfhftPRAao9haNS0WftS'}
